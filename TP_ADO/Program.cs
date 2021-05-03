8209d0c24373c2726e59e217cbcfa9e2890368fd
Valentine SCHALCKENS <v.schalckens@gmail.com>
2021-05-03 16:27:39 +0200

    avancement partie 4 tp3

--- TP_ADO/Program.cs
+++ TP_ADO/Program.cs
@@ -11,45 +11,45 @@ namespace ClientCommande
     {
         static void Main()
         {
-            ////string host = "freesio.lyc-bonaparte.fr";
-            ////int port = 21521;
-            ////string sid = "slam";
-            ////string login = "schalckensado";
-            ////string pwd = "sio";
-            //string host = "10.10.2.10";
-            //int port = 1521;
+            //string host = "freesio.lyc-bonaparte.fr";
+            //int port = 21521;
             //string sid = "slam";
             //string login = "schalckensado";
             //string pwd = "sio";
-            //try
-            //{
-            //    EmployeOracle empOracle = new EmployeOracle(host, port, sid, login, pwd);
-            //    empOracle.Ouvrir();
-            //    //empOracle.InsereCategorie("Système et réseau");
-            //    //empOracle.InsereCategorieV2("Sécurité informatique");
-            //    List<String> parametresOracle = new List<String>();
-            //    parametresOracle.Add("Développement web");
-            //    parametresOracle.Add("BR020;PHP fondamentaux;4");
-            //    parametresOracle.Add("BR081;PHP objet ;3");
-            //    parametresOracle.Add("BR082;Symphony fondamentaux ;5");
-            //    empOracle.InsereCategarieCours(parametresOracle);
+            string host = "10.10.2.10";
+            int port = 1521;
+            string sid = "slam";
+            string login = "schalckensado";
+            string pwd = "sio";
+            try
+            {
+                EmployeOracle empOracle = new EmployeOracle(host, port, sid, login, pwd);
+                empOracle.Ouvrir();
+                //empOracle.InsereCategorie("Système et réseau");
+                //empOracle.InsereCategorieV2("Sécurité informatique");
+                List<String> parametresOracle = new List<String>();
+                parametresOracle.Add("Développement web");
+                parametresOracle.Add("BR020;PHP fondamentaux;4");
+                parametresOracle.Add("BR081;PHP objet ;3");
+                parametresOracle.Add("BR082;Symphony fondamentaux ;5");
+                empOracle.InsereCategarieCours(parametresOracle);
 
-            //    parametresOracle = new List<String>();
-            //    parametresOracle.Add("Développement mobile");
-            //    parametresOracle.Add("BR060;Android fondamentaux;4");
-            //    parametresOracle.Add("BR060;Android avancé ;3");
-            //    parametresOracle.Add("BR060;Angular JS fondamentaux ;2");
-            //    empOracle.InsereCategarieCours(parametresOracle);
+                parametresOracle = new List<String>();
+                parametresOracle.Add("Développement mobile");
+                parametresOracle.Add("BR060;Android fondamentaux;4");
+                parametresOracle.Add("BR060;Android avancé ;3");
+                parametresOracle.Add("BR060;Angular JS fondamentaux ;2");
+                empOracle.InsereCategarieCours(parametresOracle);
 
-            //    empOracle.Fermer();
-            //    Console.WriteLine("--- Fin normal du Program ---");
-            //}
+                empOracle.Fermer();
+                Console.WriteLine("--- Fin normal du Program ---");
+            }
 
-            //catch (OracleException ex)
-            //{
+            catch (OracleException ex)
+            {
 
-            //    Console.WriteLine("Erreur Oracle " + ex.Message);
-            //}
+                Console.WriteLine("Erreur Oracle " + ex.Message);
+            }
 
             string hostMysql = "127.0.0.1";
             int portMysql = 3306;
